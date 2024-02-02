78b7fa5346da3c0367be34b05c494639f967f7d9
vikash pandey <ind.vikash1997@gmail.com>
2024-02-03 00:44:51 +0530

    education part done

--- src/component/right/Experence.jsx
+++ src/component/right/Experence.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { GoPencil } from "react-icons/go";
 function Experence() {
   const jobPositions = [
     {
       title: "Full Stack Developer",
       company: "",
       year: "2017-2018",
+      code: "#ec5453",
       description:
         "Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.",
     },
@@ -13,6 +14,8 @@ function Experence() {
       title: "Front End Developer",
       company: "Google Company",
       year: "2017-2018",
+      
+      code: "#f9bf3f",
       description:
         "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life. One day, however, a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.",
     },
@@ -20,6 +23,7 @@ function Experence() {
       title: "System Analyst",
       company: "",
       year: "2017-2018",
+      code: "#a84cb8",
       description:
         "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life. One day, however, a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.",
     },
@@ -27,6 +31,7 @@ function Experence() {
       title: "Creative Designer",
       company: "",
       year: "2017-2018",
+      code: "#2c98f0",
       description:
         "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life. One day, however, a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.",
     },
@@ -34,39 +39,44 @@ function Experence() {
       title: "UI/UX Designer",
       company: "Envato",
       year: "2017-2018",
+      code: "#a84cb8",
+      
       description:
         "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life. One day, however, a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.",
+    },
+    {
+    
     },
   ];
 
   return (
-
     <>
-    <div className="relative">
-    <div>
-        <p className="text-xs font-medium textp tracking-[5px] my-10">
-        EXPERIENCE
-        </p>
-        <h1 className="font-bold text-[#000] text-xl tracking-[5px]">
-        WORK EXPERIENCE
-        </h1>
-      </div>
-      <div className="xcxcxc">
-        {jobPositions.map((val,i)=>{
-          return(
-            <>
-            <div className="xxx bg-[#f2f3f7] my-3 py-5 px-10">
-              <div className="flex gap-5">
-                <h1>{val.title}</h1>
-                <p>{val.year}</p>
+      <div className="relative">
+        <div>
+          <p className="text-xs font-medium textp tracking-[5px] my-10">
+            EXPERIENCE
+          </p>
+          <h1 className="font-bold text-[#000] text-xl tracking-[5px]">
+            WORK EXPERIENCE
+          </h1>
+        </div>
+        <div className="xcxcxc">
+          {jobPositions.map((val, i) => (
+            <div key={i} className="relative">
+              <div className={i === 5 ? 'absolute left-[-47px] shadows w-8 h-8 bottom-0.5 rounded-full' : `absolute left-[-50px] shadows w-10 h-10 rounded-full `} style={{background:`${val.code}`}}>
+                {i === 5 ? null : <GoPencil className="ddddddddd iconscenter text-xl font-medium text-white" />}
               </div>
-              <p>{val.description}</p>
+              <div className={i === 5 ? 'hidden bg-white' : 'bg-[#f2f3f7] my-3 py-5 px-10'}>
+                <div className="flex gap-5">
+                  <h1>{val.title}</h1>
+                  <p>{val.year}</p>
+                </div>
+                <p>{val.description}</p>
+              </div>
             </div>
-            </>
-          )
-        })}
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 }
--- src/component/right/test.js
+++ src/component/right/test.js
@@ -127,3 +127,21 @@ const userSchema = new mongoose.Schema({
 const UserModel = mongoose.model('User', userSchema);
 
 module.exports = UserModel;
+
+
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/your-database-name', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit the process if unable to connect to MongoDB
+  }
+};
+
+module.exports = { connectToDatabase };
